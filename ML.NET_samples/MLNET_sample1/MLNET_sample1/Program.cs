cab0920e774137ef7d3de9714969761b029d9fb9
Jianping Cai <Jianping.Cai@bentley.com>
2020-10-27 17:07:26 +0800

    updates

---
+++ ML.NET_samples/MLNET_sample1/MLNET_sample1/Program.cs
@@ -0,0 +1,19 @@
+﻿using MLNET_sample1ML.Model;
+using System;
+
+namespace MLNET_sample1
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            // Add input data
+            var input = new ModelInput();
+            input.SentimentText = "That is rude.";
+
+            // Load model and predict output of sample data
+            ModelOutput result = ConsumeModel.Predict(input);
+            Console.WriteLine($"Text: {input.SentimentText}\nIs Toxic: {result.Prediction}");
+        }
+    }
+}
